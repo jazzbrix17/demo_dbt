674b6712d70deef68105ad724f8e3bff772922b3
Reynante Tusi <RTusi@vaco.com>
2025-01-29 03:14:49 +0800

    Fixed folder structure and yaml file

--- models/copy_data_from_csv.sql
+++ models/copy_data_from_csv.sql
@@ -1,4 +1,4 @@
 COPY raw_page_views (timestamp, user_id, anonymous_id)
-FROM 'dbt_workspace\dbt_ikaros_assessmentsample_page_views.csv'
+FROM 'dbt_workspace\dbt_ikaros_assessment\sample_page_views.csv'
 DELIMITER ','
 CSV HEADER;
